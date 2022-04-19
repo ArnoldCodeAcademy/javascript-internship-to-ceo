1208a9ee5520f42bc9d40a6cac7bb5623bbea5d2
Arnold Abraham <info@arnoldcode.com>
2022-04-19 21:12:54 +0200

    #02 Solution to Challenge.

--- challengeHere.js
+++ challengeHere.js
@@ -1,23 +1,49 @@
 // Solution #01
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 99; i += 2) {
     console.log(i);
 }
 
 // Get into the Community https://www.facebook.com/groups/webdevconquerors me on Facebook and don’t miss the latest Challenge!
 
-/* EXPLANATION
+/* EXPLANATION - Way #1
 
-  You need to print out ten numbers to the console prompt.
+    To get the expected result, you start the running index i with the value of 1 and have in mind that in between every odd number comes an even number.
 
-  A `for-loop` is the perfect tool for this job!
+    Therefore, you increase your running index i by 2 every iteration with i += 2.
 
-  You create a loop with a running index of `i` with the starting value of `1`.
+    You set the condition to <=99 because you only want to print out odd numbers below 99, that means the odd numbers between 1 to 97.
 
-  The loop will stop once `i` reaches the value of `11`, so when `i` equals `10`, the loop will execute the last time because you created the condition of    `i <= 10`.
+    Inside the loop you got your statement of console.log(i); which prints out, whatever is inside the brace, to the console window.
 
-  The running index will be increased by `1` every iteration. `i++` is the corresponding statement for that. It is the short form for `i = i+1`.
+    In your case the wanted odd number.
+ */
 
-  Inside the loop you got your statement of `console.log(i);` which prints out, whatever is inside the brace, to the console window.
+// Get into the Community https://www.facebook.com/groups/webdevconquerors me on Facebook and don’t miss the latest Challenge!
+
+// Solution #02
+for (let i = 1; i <= 99; i += 2) {
+    console.log(i);
+}
+
+// Get into the Community https://www.facebook.com/groups/webdevconquerors me on Facebook and don’t miss the latest Challenge!
+
+/* EXPLANATION - Way #2
+
+    When starting from 97, which is an odd number respectively, and iterate backwards, you almost got it.
+
+    To get every odd number, remember yourself that in between every odd number comes an even number.
+
+    Jumping over them by decreasing the running index i to get every odd number.
+
+    You have to change the conditions for the for-loop.
+
+    First i starts at 97, then your condition has to be i > 0. Not i >= 0 , otherwise you would print out the even number 0 (Yes 0 is an even number).
+
+    The running index gets decreased by 2 after every iteration with i -= 2.
+
+    Inside the loop you got your statement of console.log(i); which prints out, whatever is inside the brace, to the console window.
+
+    In your case the wanted odd number.
 
  */
 
