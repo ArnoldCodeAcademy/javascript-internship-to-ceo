ac45bd782381ffd88e27de8ad70ea514919f8a4f
Arnold Abraham <info@arnoldcode.com>
2022-04-19 21:09:44 +0200

    #01 Solution to Challenge.

--- challengeHere.js
+++ challengeHere.js
@@ -1,1 +1,24 @@
-// Write Your Code Below This Line
+// Solution #01
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Get into the Community https://www.facebook.com/groups/webdevconquerors me on Facebook and don’t miss the latest Challenge!
+
+/* EXPLANATION
+
+  You need to print out ten numbers to the console prompt.
+
+  A `for-loop` is the perfect tool for this job!
+
+  You create a loop with a running index of `i` with the starting value of `1`.
+
+  The loop will stop once `i` reaches the value of `11`, so when `i` equals `10`, the loop will execute the last time because you created the condition of    `i <= 10`.
+
+  The running index will be increased by `1` every iteration. `i++` is the corresponding statement for that. It is the short form for `i = i+1`.
+
+  Inside the loop you got your statement of `console.log(i);` which prints out, whatever is inside the brace, to the console window.
+
+ */
+
+// Get into the Community https://www.facebook.com/groups/webdevconquerors me on Facebook and don’t miss the latest Challenge!
